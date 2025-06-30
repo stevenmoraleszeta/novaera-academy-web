fe343bb7def45c43c7ce1c00d93bde02a98a79fd
Esteven <ehsteven@gmail.com>
2025-06-30 13:51:10 -0600

    `Added form validation and error handling to ClassesRecorded component`

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -31,68 +31,6 @@ function Navbar() {
         { path: '/cursos-en-vivo', label: 'CURSOS EN VIVO' },
         { path: '/servicios', label: 'SERVICIOS' },
     ];
-    /*
-    return (
-        <div className={styles.topnav}>
-            <button
-                className={styles.hamburger}
-                onClick={() => setMenuOpen(!menuOpen)}
-                aria-label="Toggle navigation"
-            >
-                ☰
-            </button>
-            <Link href={'/'} className={styles.imgLink}>
-                <Image
-                    alt="ZetaLogo"
-                    src="https://firebasestorage.googleapis.com/v0/b/zeta-3a31d.appspot.com/o/images%2FZetaLogo.png?alt=media&token=d8e33971-ceb0-4d9e-a617-2f026fe4467c"
-                    width={100}
-                    height={100}
-                    className={styles.zLogo}
-                />
-            </Link>
-            <ul className={`${styles.navLinks} ${menuOpen ? styles.showMenu : ''}`}>
-                {navItems.map((item) => (
-                    <li key={item.path}>
-                        <Link
-                            href={item.path}
-                            className={
-                                pathName === item.path
-                                    ? styles.navbarLinkSelected
-                                    : styles.navbarLink
-                            }
-                            onClick={() => setMenuOpen(false)} // Cierra el menú al hacer clic
-                        >
-                            {item.label}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-            <div className={styles.manageContainer}>
-                {isAdmin && currentUser && (
-                    <Link href="/admin" className={styles.accessLink}>
-                        ADMIN
-                    </Link>
-                )}
-                {currentUser ? (
-                    <Link href="/userProfile" className={styles.profileLink}>
-                        <Image
-                            className={styles.profileImage}
-                            src={profileImage || defaultProfileImage}
-                            alt="Profile"
-                            width={40}
-                            height={40}
-                        />
-                    </Link>
-                ) : (
-                    <Link href="/login" className={styles.accessLink}>
-                        ACCEDER
-                    </Link>
-                )}
-            </div>
-        </div>
-    );
-    
-    */
 
     return (
         // CAMBIO: Usando la clase global "topnav" como string
@@ -156,7 +94,7 @@ function Navbar() {
                         />
                     </Link>
                 ) : (
-                    <Link href="/login" className={styles.accessLink}>
+                    <Link href="/admin" className= "accessLink">
                         ACCEDER
                     </Link>
                 )}
